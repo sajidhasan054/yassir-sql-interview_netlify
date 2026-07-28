4f8ca56ba65dbb312b1e041fc3205da024d5b1c9
Sajid Hasan Sifat <sajidhasan054@gmail.com>
2026-07-28 22:08:11 +0600

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ window.YASSIR_CONFIG = {
     ENABLE_NETLIFY_FORMS: true,
 
     // Deployed Google Apps Script Endpoint for Real-time Database Sync
-    GOOGLE_SHEETS_URL: "https://script.google.com/macros/s/AKfycbzIs6fJB1hTl8jE8r5t5YbnDa3qMbL471Y3o-ssq1DNdT07GKI-jgz5eBF36pXBCw7i/exec"
+    GOOGLE_SHEETS_URL: "https://script.google.com/macros/s/AKfycbxCyqa0DUX98WhWR-K8cfj-sOpo_gwxFHsAIieu1CDg4oOPEnaRitPE9UKTKXhkeB_T/exec"
 };
